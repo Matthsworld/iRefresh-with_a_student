10d55d2c7859e00b5757f8bf2d1da769afefc499
matthsworld <matthsworld@gmail.com>
2022-09-10 15:04:05 -0700

    feat: add loops.py file

---
+++ HR-quiz_solution/loops.py
@@ -0,0 +1,20 @@
+'''
+Task
+The provided code stub reads and integer, n, from STDIN. For all non-negative integers I<n, print i^2.
+
+Example
+n = 3
+The list of non-negative integers that are less than n = 3 is [0, 1, 2]. Print the square of each number on a separate line.
+
+0
+1
+4
+'''
+
+Solution
+
+if __name__ == '__main__':
+    n = int(input())
+    i = range(n)
+for i in range(n):
+    print(i**2)
